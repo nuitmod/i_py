b07c49898bfda8b13a6c88f6fc33265877d5296d
nuitmod <wtvfqm@gmail.com>
2021-04-28 15:50:06 +0500

    add y2

--- components/main.ts
+++ components/main.ts
@@ -1,13 +1,22 @@
-import { html, render } from '../modules/preact_link.js';
+import { html, Component, render } from  '../modules/preact_link.js'
+import { observer } from '../modules/mobx_preact.module.js';
+//import imob from "./mobx_store.js";
+import { autorun, toJS } from '../modules/mobx.module.js';
 //import  * as jQuery from "https://unpkg.com/jquery@3.5.1/dist/jquery.min.js";
+
+
 //var $ = window.jQuery;
+
 console.log("main file");
-let str_dat = "main ts data";
-let main = () => html `
+
+
+let str_dat="main ts data"
+let main=()=>html`
 <div>
     <amp-bind-macro id="repl2" arguments="str" expression="str.toLowerCase()">
     </amp-bind-macro>
     <input type="text" placeholder="dat" on="input-throttled:AMP.setState({ arg: event.value })">
     <div data-amp-bind-text="'Input is ' + repl2(arg)">${str_dat}</div>
-</div>`;
-render(main(), document.getElementById("main"));
+</div>`
+
+render(main(), document.getElementById("main"))
