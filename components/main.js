9ad5b0d9cc452b3a52f9ba939dc7c5c4d09929e1
nuitmod <wtvfqm@gmail.com>
2021-06-03 15:32:15 +0500

    return main ts

--- components/main.js
+++ components/main.js
@@ -1,13 +1,12 @@
 import { html, render } from '../modules/preact_link.js';
-//import  * as jQuery from "https://unpkg.com/jquery@3.5.1/dist/jquery.min.js";
 //var $ = window.jQuery;
 console.log("main file");
 let str_dat = "main ts data";
 let main = () => html `
-<div>
+  <div>
     <amp-bind-macro id="repl2" arguments="str" expression="str.toLowerCase()">
     </amp-bind-macro>
     <input type="text" placeholder="dat" on="input-throttled:AMP.setState({ arg: event.value })">
     <div data-amp-bind-text="'Input is ' + repl2(arg)">${str_dat}</div>
-</div>`;
+  </div>`;
 render(main(), document.getElementById("main"));
--- components/main.ts
+++ components/main.ts
@@ -3,20 +3,31 @@ import { observer } from '../modules/mobx_preact.module.js';
 //import imob from "./mobx_store.js";
 import { autorun, toJS } from '../modules/mobx.module.js';
 //import  * as jQuery from "https://unpkg.com/jquery@3.5.1/dist/jquery.min.js";
-
+import * as jQuery from '../modules/jquery.min.js';
 
 //var $ = window.jQuery;
 
+
 console.log("main file");
 
+let str_dat="main ts data";
 
-let str_dat="main ts data"
 let main=()=>html`
-<div>
+  <div>
     <amp-bind-macro id="repl2" arguments="str" expression="str.toLowerCase()">
     </amp-bind-macro>
     <input type="text" placeholder="dat" on="input-throttled:AMP.setState({ arg: event.value })">
     <div data-amp-bind-text="'Input is ' + repl2(arg)">${str_dat}</div>
-</div>`
+  </div>`
+
+
+
+
+
+
+
+
+
+
 
 render(main(), document.getElementById("main"))
