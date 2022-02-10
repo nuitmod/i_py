5474c9ce76ec3f2ed0a3ff52bda487f19bf69b39
nuitmod <wtvfqm@gmail.com>
2022-02-10 11:56:58 +0500

    add amp script

--- components/header.ts
+++ components/header.ts
@@ -14,12 +14,13 @@ let name: string="i-py pr";
 var header=()=>html`
   <amp-accordion class="menu" disable-session-states>
     <section>
-      <h5 class="amp_a">${name}</h5>
+      <h6 class="amp_a">${name}</h6>
       <div>
         <a href="https://nuitmod.github.io/ini_pwa/amp/">ini_pwa</a>
         <a href="https://nuitmod.github.io/npx_io/">npx_io</a>
         <a href="https://nuitmod.github.io/io_proj/amp/">io_proj</a>
         <a href="https://nuitmod.github.io/mod_i/amp/">mod_i</a>
+        <a href="https://nuitmod.github.io/mod_amp/amp/">mod_amp</a>
         <a href="https://i-nuit.vercel.app/">i-nuit</a>
       </div>
     </section>
